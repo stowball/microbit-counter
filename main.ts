e3aa0930e1998d664aaf59c60ad391f9ceda4ef5
Matt Stow <matt@mattstow.com>
2026-03-23 21:04:49 +1100

    Update pxt.json, main.blocks, main.ts, pxt.json, _history

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 input.onButtonPressed(Button.A, function () {
+    Count += 10
+})
+input.onButtonPressed(Button.B, function () {
     Count += 1
 })
 let Heart = false
-let Count = 3021
+let Count = 3042
 basic.forever(function () {
     basic.showString("" + (Count))
     if (Heart) {
